6b99d2bc2245192d5b3faf09db3153197c30896a
Stuart Zilm <stuart.zilm@gmail.com>
2010-09-17 12:14:41 -0400

    Image named scopes for today, yesterday, latest and recent.

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,10 @@ class Image < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   
   has_attached_file :data, :styles => { :thumb => '64x64>', :icon => '32x32#' }
+  
+  scope :today, where(:date => Time.now)
+  scope :yesterday, where(:date => 1.day.ago)
+  scope :latest, order(:date).reverse_order.limit(1)
+  
+  scope :recent, where('date >= ?', 1.day.ago).order(:date).reverse_order.limit(1)
 end

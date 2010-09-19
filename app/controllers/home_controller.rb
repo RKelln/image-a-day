e9e85ae4f48ee550dc3ee7df6e8e20bd13203630
Ryan Kelln <ryankelln@gmail.com>
2010-09-19 12:22:46 -0400

    integrate Ad-Gallery js+stylesheets+images, homepage is now test for gallery

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @user = current_user
+    @images = @user.images.order('date DESC')
   end
 
 end

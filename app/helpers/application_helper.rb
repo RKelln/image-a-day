607cc4583b4123246ea6dcc8b88159a55b276823
Stuart Zilm <stuart.zilm@gmail.com>
2010-09-20 16:26:28 -0400

    Comment edits work correctly via edit_subject_path application helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,17 @@ module ApplicationHelper
   def site_title()
     SITE_TITLE
   end
+  
+  def edit_subject_path(subject)
+    if subject.is_a? Image
+      # FIXME: images cannot yet be edited
+      #return edit_image_path(subject)
+      return nil
+    elsif subject.is_a? Comment
+      return edit_comment_path(subject)
+    else
+      # FIXME: this should return something more erroneous
+      return nil
+    end
+  end
 end

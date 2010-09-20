bb22dc0ba2a25e7cbc62e3b6279ca10949026f15
Stuart Zilm <stuart.zilm@gmail.com>
2010-09-20 16:45:09 -0400

    upload_path helper provide upload paths by date.

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,9 @@
 module ImagesHelper
+  def upload_path(date=nil)
+    if date
+      return "/images/upload/#{date.year}/#{date.month}/#{date.day}"
+    else
+      return upload_images_path
+    end
+  end
 end

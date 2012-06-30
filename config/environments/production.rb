6c1c59aa183ad8631ae212f7a173f9ee5ed22163
Ryan Kelln <ryankelln@gmail.com>
2012-06-30 18:20:40 -0300

    Fix for static images not being served (0 bytes error)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,9 @@ ImageADay::Application.configure do
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # RK: disabled since images were not being loaded properly:
+  # http://stackoverflow.com/questions/6458947/rails-3-1-heroku-cedar-static-image-assets-are-not-being-served
 
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
